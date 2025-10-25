7dfdd33ca3ef5ff832c0a2ab1fd64b8742710e58
Thais Nuñez <thaiskenobi@gmail.com>
2025-10-25 18:58:26 +0200

    Mejora con imagen el html

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ const modulos = document.querySelectorAll("#modulos li");
 const mensaje = document.getElementById("mensaje");
 
 modulos.forEach((modulo) => {
-	modulo.addEventListener("click", () => {
-	mensaje.textContent = `Has seleccionado: ${modulo.textContent}`;
-	});
+  modulo.addEventListener("click", () => {
+    mensaje.textContent = `Has seleccionado: ${modulo.textContent}`;
+    mensaje.style.opacity = "0";
+    setTimeout(() => (mensaje.style.opacity = "1"), 200);
+
+    // Pequeño efecto al hacer clic
+    modulo.style.color = "#004080";
+    modulo.style.fontWeight = "bold";
+    setTimeout(() => {
+      modulo.style.fontWeight = "normal";
+    }, 600);
+  });
 });
+
